9af38401d778443d0e6031d5c388b7561cda9a22
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-10-13 00:03:48 -0400

    (feat) add test & method addShip in gameBoard.
    -> tests check the boats are added vertically and horizontally on the board.
    
    -> method adds a new Ship object in each index from the start coordinates to the end.

--- src/javascript/gameboard.js
+++ src/javascript/gameboard.js
@@ -1,5 +1,23 @@
+import { Ship } from './ships.js';
+
 export class gameBoard {
 	board = createBoard();
+
+	addShip(startCord, endCord, boatSize) {
+		let [startX, startY] = startCord;
+		let board = this.board;
+		let queue = [board[startX][startY]];
+		let direction = boatDirection(startCord, endCord);
+		for (let i = 1; i <= boatSize; i++) {
+			queue[0].value = new Ship(5);
+			if (direction == 'horizontal') {
+				queue.push(board[startX][startY + i]);
+			} else {
+				queue.push(board[startX + i][startY]);
+			}
+			queue.splice(0, 1);
+		}
+	}
 }
 
 function createBoard() {
@@ -13,4 +31,12 @@ function createBoard() {
 		}
 	}
 	return board;
+}
+
+function boatDirection(startCord, endCord) {
+	let [startX] = startCord;
+	let [endX] = endCord;
+
+	if (startX == endX) return 'horizontal';
+	return 'vertical';
 }
--- src/javascript/gameboard.test.js
+++ src/javascript/gameboard.test.js
@@ -1,14 +1,35 @@
 import { gameBoard } from './gameboard.js';
 
 describe('Game Board', () => {
+	let newBoard = new gameBoard();
 	it('Has 10x10 Board', () => {
-		let newBoard = new gameBoard();
 		let length = newBoard.board.every((row) => row.length == 10);
 		expect(length).toBe(true);
 		expect(newBoard.board.length).toBe(10);
-
 		for (let i = 0; i < newBoard.board.length; i++) {
 			newBoard.board[i].every((column) => expect(column).toEqual({ value: '' }));
+		}
+	});
+
+	it('Adds ship horizontally', () => {
+		newBoard.addShip([0, 0], [0, 5], 5);
+		for (let i = 0; i < 5; i++) {
+			expect(newBoard.board[0][i].value).toEqual({
+				length: 5,
+				timesHit: 0,
+				sunk: false,
+			});
+		}
+	});
+
+	it('Adds ship vertically', () => {
+		newBoard.addShip([1, 0], [5, 0], 5);
+		for (let i = 0; i < 5; i++) {
+			expect(newBoard.board[1 + i][0].value).toEqual({
+				length: 5,
+				timesHit: 0,
+				sunk: false,
+			});
 		}
 	});
 });

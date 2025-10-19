700f8c947d2a098d7cfb6b7cfcd8ce2d274ec0b7
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-10-18 23:47:51 -0400

    (refactor) populateBoard & createPlayers
    -> populateBoard now just populates one board and
    receives one player object. It will then call its helper function
    of rendering the board with the board from the parameter and the name
    from that player object.
    
    -> create players changed default naming and parameter names.
    
    -> within the module the attacks are now called outside the functions
    to fulfill the step 2 from the project.

--- src/javascript/gamecontroller.js
+++ src/javascript/gamecontroller.js
@@ -1,28 +1,33 @@
 import { Player } from './player.js';
 import { renderBoard } from './UI.js';
 
-function createPlayers(player = 'user', computer = 'cpu') {
-	const user = new Player(player);
-	const cpu = new Player(computer);
-	return { user, cpu };
+function createPlayers(
+	playerOneName = 'Player One',
+	playerTwoName = 'Player Two'
+) {
+	const playerOne = new Player(playerOneName);
+	const playerTwo = new Player(playerTwoName);
+	return { playerOne, playerTwo };
 }
 
-export function populateBoards() {
-	let players = createPlayers();
-	let userBoard = players.user.game.board;
-	let cpuBoard = players.cpu.game.board;
+export let players = createPlayers();
+let numberOne = players.playerOne.game;
+let numberTwo = players.playerTwo.game;
 
-	players.user.game.addShip([0, 0], [4, 0], 5);
-	players.user.game.addShip([5, 5], [8, 5], 4);
-	players.user.game.addShip([3, 7], [3, 9], 3);
-	players.user.game.addShip([7, 2], [9, 2], 3);
-	players.user.game.addShip([1, 8], [1, 9], 2);
-	renderBoard(userBoard);
+numberOne.addShip([0, 0], [4, 0], 5);
+numberOne.addShip([5, 5], [8, 5], 4);
+numberOne.addShip([3, 7], [3, 9], 3);
+numberOne.addShip([7, 2], [9, 2], 3);
+numberOne.addShip([1, 8], [1, 9], 2);
 
-	players.cpu.game.addShip([0, 0], [4, 0], 5);
-	players.cpu.game.addShip([5, 5], [8, 5], 4);
-	players.cpu.game.addShip([3, 7], [3, 9], 3);
-	players.cpu.game.addShip([7, 2], [9, 2], 3);
-	players.cpu.game.addShip([1, 8], [1, 9], 2);
-	renderBoard(cpuBoard);
+numberTwo.addShip([0, 0], [4, 0], 5);
+numberTwo.addShip([5, 5], [8, 5], 4);
+numberTwo.addShip([3, 7], [3, 9], 3);
+numberTwo.addShip([7, 2], [9, 2], 3);
+numberTwo.addShip([1, 8], [1, 9], 2);
+
+export function populateBoard(player) {
+	let playerBoard = player.game.board;
+	let playerName = player.name;
+	renderBoard(playerBoard, playerName);
 }

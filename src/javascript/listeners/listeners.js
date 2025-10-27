9ebfa8ac1e38dcb6acb0b654531784137d0a92be
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-10-27 00:43:12 -0400

    (feat) add confirmBtnListener fn
    -> this function is needed since when clicking randomize button for
    the first time, createBtn function is called and it creates a new
    button confirm.
    - this fn renders the next board and if the opponent is real,
    a randomize button will be created otherwise the game will start and
    a boardListener is set.
    
    (refactor) due to removing populateBoard, all those functions were
    replaced with renderBoard

--- src/javascript/listeners/listeners.js
+++ src/javascript/listeners/listeners.js
@@ -1,6 +1,5 @@
 import {
 	createPlayers,
-	populateBoard,
 	randomizeBoatsPosition,
 } from '../controller/gamecontroller';
 import {
@@ -10,7 +9,7 @@ import {
 	removeBtns,
 	setPlayerInfoText,
 } from '../ui/elements';
-import { cpuPlays, removeBoard, removeElements, renderBoard } from '../ui/ui';
+import { renderBoard, removeBoard } from '../ui/ui.js';
 
 export function boardListener(playerObject, DomBoard = 'player-two') {
 	const allSquares = document.querySelectorAll(`.${DomBoard} li`);
@@ -28,7 +27,7 @@ export function menuSelectionListener() {
 			let players = createPlayers(element.value);
 			cleanMainElement();
 			gameElements();
-			renderBoard(players.playerOne.game.board, 'player-one');
+			renderBoard(players.playerOne);
 			setPlayerInfoText('Place your ships!');
 			createBtn('Randomize', 'randomize-button', 'player-one');
 			randomizeBtnListener(players);
@@ -43,11 +42,42 @@ function randomizeBtnListener(playerObject) {
 		removeBoard();
 		randomizeBoatsPosition(playerObject, player);
 		if (player == 'player-one') {
-			populateBoard(playerObject.playerOne);
+			renderBoard(playerObject.playerOne);
 		} else {
-			populateBoard(playerObject.playerOne);
+			renderBoard(playerObject.playerTwo);
 		}
 		removeBtns();
 		createBtn('Confirm', 'confirm-button', player);
+		confirmBtnListener(playerObject);
 	});
 }
+
+function confirmBtnListener(playerObject) {
+	const confirmBtn = document.querySelector('.confirm-button');
+	let currentPlayer = confirmBtn.classList[1];
+	confirmBtn.addEventListener('click', () => {
+		// debugger;
+		let player = confirmBtn.classList[1];
+		removeBoard();
+		removeBtns();
+		renderBoard(playerObject.playerTwo);
+		if (playerObject.playerTwo.name == 'cpu') {
+			randomizeBoatsPosition(playerObject, player);
+			renderBoard(playerObject.playerTwo);
+			boardListener(playerObject);
+			return;
+		} else if (currentPlayer == 'player-one') {
+			createBtn('Randomize', 'randomize-button', 'player-two');
+			randomizeBtnListener(playerObject);
+		} else {
+			// removeBtns();
+			// removeBoard();
+			renderBoard(playerObject.playerTwo);
+			createBtn('Confirm', 'confirm-button', player);
+			confirmBtnListener(playerObject);
+		}
+	});
+}
+
+/*  Problem: 
+	the last confirmation doubles the grid instead of clearing the old one */

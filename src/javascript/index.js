363f3444a85cc21821436c32d0e4d950b2b64fd4
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-11-08 18:21:17 -0500

    (refactor) add menuSelection and its listener
    -> remove comments and leave the two functions
    that are needed to start the game.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,42 +1,10 @@
 import '../css/style.css';
-import '../css/board.css';
+import '../css/game-section.css';
 import '../css/reset.css';
-import {
-	populateBoard,
-	players,
-	randomizeBoatsPosition,
-} from './controller/gamecontroller.js';
-import { boardListener } from './ui/ui.js';
+import '../css/menu-selection.css';
+import '../css/drag-drop.css';
+import { menuSelection } from './ui/elements';
+import { menuSelectionListener } from './listeners/listeners';
 
-randomizeBoatsPosition(players);
-randomizeBoatsPosition(players, 'player-one');
-
-populateBoard(players.playerOne);
-populateBoard(players.playerTwo);
-
-boardListener(players);
-
-/* 
-1-  the first thing that shows is the selection of players:
-	- vs computer
-	- vs player 2
-
-2 - place the ships:
-	- randomize
-	- drag and drop
-
-3 - start playing by clicking on square:
-	- have the computer being slower to see its play in slow mo.
-	- adversary board should not display boats only
-	- own board should display boats.
-
-4 - game ends when all ships are sunk:
-	- show winning message
-	- show buttons for new game & replay.
-
-5 - possible but not mandatory:
-	- flipping boards when player changes
-	- improve the computer AI
-
-
-*/
+menuSelection();
+menuSelectionListener();
